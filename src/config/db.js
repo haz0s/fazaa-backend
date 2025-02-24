eef03823baab68e99e98ce54b5b966cf5accadf4
haz0s <hazos2005@gmail.com>
2025-02-24 15:47:56 +0000

    postgreinit

--- src/config/db.js
+++ src/config/db.js
@@ -1,23 +1,21 @@
-// src/config/db.js
+const { Client } = require('pg');
 
-const { Pool } = require("pg");
-require("dotenv").config(); // Make sure to require dotenv to access environment variables
-
-const pool = new Pool({
-  user: process.env.DB_USER,
+// Use environment variables for connection details
+const client = new Client({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }, // For SSL connection
 });
 
-// Test the connection
-pool.connect((err) => {
-  if (err) {
-    console.error("Failed to connect to the database", err);
-  } else {
-    console.log("Connected to the database");
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to the database');
+  } catch (err) {
+    console.error('Connection error', err.stack);
   }
-});
-
-module.exports = pool;
+}
+module.exports = { client, connectToDatabase };
